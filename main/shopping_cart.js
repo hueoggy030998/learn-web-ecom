71863132085ced3ce7d237b2c8d33f16d00b71fe
hueoggy030998 <huetransky@gmail.com>
2024-11-07 16:02:25 +0700

    show cart total

--- main/shopping_cart.js
+++ main/shopping_cart.js
@@ -1,18 +1,23 @@
 const listProductsContainer = document.getElementById("shopping-cart-list-products");
 const clearCartBtn = document.getElementById("clear-cart-btn");
+const subtotalsValue = document.querySelector(".cart-totals-container .subtotal-value");
+const totalsValue = document.querySelector(".cart-totals-container .total-value");
 showCart();
 function showCart(){
     // lay du lieu tu localStorage
     let arrCart = getCart();
     listProductsContainer.innerHTML = "";
     if(arrCart && arrCart.length > 0){
+        let subTotal = 0;
         for (let i=0; i<arrCart.length; i++) {
+            subTotal += arrCart[i].quantity * arrCart[i].price;
             const productItem = createProductItem(arrCart[i]);
             listProductsContainer.appendChild(productItem);
             const clearProductItem = productItem.querySelector(".product-thumb-close");
+            const productTotal = productItem.querySelector(".product-total");
             clearProductItem.addEventListener("click", function(){
                 removeCart(arrCart, i);
-                showCart();
+                listProductsContainer.removeChild(productItem);
             })
 
             const decreaseProductItem = productItem.querySelector(".decrease-button");
@@ -20,21 +25,53 @@ function showCart(){
             const increaseProductItem = productItem.querySelector(".increase-button");
             decreaseProductItem.addEventListener("click", function(){
                 quantityInput.value=quantityInput.value-1;
+                subTotal = subTotal - arrCart[i].price;
+                showTotal(subTotal);
                 updateCart(arrCart, i, +quantityInput.value);
                 if(quantityInput.value <= 0){
                     removeCart(arrCart, i);
-                    showCart();
+                    listProductsContainer.removeChild(productItem);
+                    if(arrCart.length===0){
+                        listProductsContainer.outerHTML = `<div class="josefin-sans no-data">No data</div>`;
+                    }
+                } else {
+                    productTotal.innerHTML = `$${(quantityInput.value * arrCart[i].price).toLocaleString("en-US")}`;
                 }
             });
             increaseProductItem.addEventListener("click", function(){
                 quantityInput.value=+quantityInput.value+1;
+                subTotal = subTotal + arrCart[i].price;
+                showTotal(subTotal);
                 updateCart(arrCart, i, +quantityInput.value);
+                productTotal.innerHTML = `$${(quantityInput.value * arrCart[i].price).toLocaleString("en-US")}`;
             });
+
+            quantityInput.addEventListener("input", function(){
+                if(!isNaN(quantityInput.value) && quantityInput.value.trim() !== '' && quantityInput.value != 0){
+                    subTotal = subTotal - (arrCart[i].quantity * arrCart[i].price) + (+quantityInput.value * arrCart[i].price);
+                    showTotal(subTotal);
+                    updateCart(arrCart, i, +quantityInput.value);
+                    productTotal.innerHTML = `$${(quantityInput.value * arrCart[i].price).toLocaleString("en-US")}`;
+                }else{
+                    subTotal = subTotal - (arrCart[i].quantity * arrCart[i].price) + (+quantityInput.value * arrCart[i].price);
+                    showTotal(subTotal);
+                    quantityInput.value=1;
+                    updateCart(arrCart, i, +quantityInput.value);
+                    productTotal.innerHTML = `$${(quantityInput.value * arrCart[i].price).toLocaleString("en-US")}`;
+                }
+            })
         }
+        showTotal(subTotal);
+        
     }else{
         listProductsContainer.outerHTML = 
         `<div class="josefin-sans no-data">No data</div>`;
     }
+}
+
+function showTotal(subTotal){
+    subtotalsValue.textContent = `$${subTotal.toLocaleString("en-US")}`;
+    totalsValue.textContent = `$${subTotal.toLocaleString("en-US")}`;
 }
 
 function createProductItem(product){
@@ -56,7 +93,7 @@ function createProductItem(product){
     <td class="product-price">$${product.price.toLocaleString("en-US")}</td>
     <td class="product-quantity">
         <button type="button" class="decrease-button">-</button>
-        <input type="number" value="${product.quantity.toLocaleString("en-US")}">
+        <input type="text" value="${product.quantity.toLocaleString("en-US")}">
         <button type="button" class="increase-button">+</button>
     </td>
     <td class="product-total">$${(product.quantity * product.price).toLocaleString("en-US")}</td>                
